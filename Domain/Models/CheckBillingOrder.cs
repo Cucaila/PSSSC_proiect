4db32d625139e3e47a807d411e6dcb229b8a16d4
Adina <adina.cucaila@student.upt.ro>
2024-01-10 22:42:05 +0200

    +checkbillingorder

---
+++ Domain/Models/CheckBillingOrder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public record CheckBillingOrder(OrderRegistrationCode OrderRegistrationCode, IReadOnlyCollection<UnvalidatedBillingOrder> UnvalidatedCustomerOrdersList);
+}
